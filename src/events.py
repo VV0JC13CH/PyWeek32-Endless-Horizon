07756f4c6c3f88b75824bec603802c09432ada6e
wojciech <wojciech@bitstudio.dev>
2021-09-26 12:01:28 +0200

    quick fixes before end of pyweek

--- src/events.py
+++ src/events.py
@@ -6,11 +6,11 @@ TEXT_EVENTS = [(0, ""), (1, "HELLO"), (2, "THIS IS..."), (3, "PyWeek32 Entry"), 
                (5, "Nice Weather Tho"), (6, "Music also sounds fine"), (7, "Where was I?"),
                (8, "You see this progress bar above?"), (9, "I think we gonna make it"),
                (10, "This duck is strong."), (11, "but be careful..."),
-               (12, "do not lose the cursor"), (13, ""), (16, "Uff. Cursor is back again."),
-               (17, ""), (18, "What was that?"), (19, ""),
+               (12, "do not lose the cursor"), (13, ""), (16, "Magic."),
+               (17, ""), (18, ""), (19, "What was that?"),
                (20, "I didn't mention something before"), (21, "Something important"), (22, "You can't loose"),
                (23, "but!"), (24, "It's possible to win!"), (25, ""),
-               (28, "Ducks ahead"), (29, "Hold your mouse"), (30, ""),
+               (28, "The ducks have found you"), (29, "Don't move mouse"), (30, ""),
                (31, "Yeah...you still wanna play?"), (32, "This game is unfinished"),
                (33, "But at the end"), (34, "There is nice image"), (35, "I mean"),
                (36, "victory screen"), (37, ""), (38, "No, no more content."),
@@ -19,7 +19,14 @@ TEXT_EVENTS = [(0, ""), (1, "HELLO"), (2, "THIS IS..."), (3, "PyWeek32 Entry"), 
                (44, "time will show"), (45, ""),
                (46, "btw"), (47, "Thanks for playing."), (48, "I appreciate it"),
                (49, ""), (50, "It took me one hour"), (51, "To make this cursor disappearing"),
-               (52, "It's not magic"), (53, "only hard work"), (54, "")]
+               (52, "It's not magic"), (53, "only hard work"), (54, ""), (56, "More boxes"),
+               (57, ""), (58, ""), (62, "One day"), (63, "I will finish this game"),
+               (64, ""), (75, "Still here?"), (76, ""), (82, "THE END"), (84, "Joke"),
+               (85, ""), (87, "Road is long"), (88, "or maybe the sea?"), (89, ""),
+               (90, "for sure"), (91, "sea is the limit"), (92, ""), (100, "no more content ahead"),
+               (101, "really"), (102, ""), (105, "only boxes"), (106, ""), (109, "and ducks"),
+               (110, ""), (120, "bye"), (121, "I have to go"), (122, "Once again, thanks for playing."),
+               (123, "")]
 
 
 class EventManager:
@@ -54,10 +61,14 @@ class EventManager:
                                 (13, "1_CURSOR"),
                                 (14, "1_CURSOR"),
                                 (15, "1_CURSOR"),
-                                (17, "2_SPAWN_BOXES"),
+                                (18, "2_SPAWN_BOXES"),
+                                (19, "4_SPAWN_BOXES_MORE"),
                                 (29, "3_SPAWN_DUCKS"),
                                 (51, "1_CURSOR"),
                                 (53, "1_CURSOR"),
+                                (56, "4_SPAWN_BOXES_MORE"),
+                                (105, "4_SPAWN_BOXES_MORE"),
+                                (110, "3_SPAWN_DUCKS")
                                 ]
 
     def on_update(self):
@@ -77,6 +88,8 @@ class EventManager:
                         self.spawn_boxes()
                     elif event[1] == "3_SPAWN_DUCKS":
                         self.spawn_ducks = True
+                    elif event[1] == "4_SPAWN_BOXES_MORE":
+                        self.spawn_more_boxes()
                     print("Game scripted event:", event[1])
                     self.script_executed = True
                     self.last_scripted_event_position = self.window.view_game.camera_sprites.position[0]
@@ -120,6 +133,13 @@ class EventManager:
         elements.make_crate(self.window.view_game.camera_sprites.position[0],
                             self.mouse_world_y,
                             self.space, self.global_sprite_list)
+
+    def spawn_more_boxes(self):
+        for i in range(0,50):
+            elements.make_crate(random.randrange(int(self.window.view_game.camera_sprites.position[0]-100),
+                                                 int(self.window.view_game.camera_sprites.position[0])),
+                                random.randrange(int(self.window.height * 0.2), int(self.window.height * 0.6)),
+                                self.space, self.global_sprite_list)
 
     def spawn_duck(self, velocity):
         elements.make_duck(x=self.window.view_game.camera_sprites.position[0],
